170b1b740e6cfeb7c0652f460ee917c3a4b81c78
Alekzanther <alexanderlndbrg@gmail.com>
2024-05-05 22:22:11 +0200

    Alignment and such

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b to-[#000] from-[#261132] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <SignedOut>
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
