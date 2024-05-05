e73fcfcd973fbb118f156fa47620eddded3019a8
Alekzanther <alexanderlndbrg@gmail.com>
2024-05-05 22:07:05 +0200

    Display different things depending on logged in state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          lucid
-        </h1>
+        <SignedOut>
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            lucid
+          </h1>
+          <p className="text-lg text-center text-gray-300">
+            OKRs. Connect work to impact. From start to summit.
+          </p>
+        </SignedOut>
+        <SignedIn>
+          <h1 className="font-extrabold tracking-tight text-white sm:text-[5rem]">
+            An ambitious and inspiring Objective
+          </h1>
+          <div className="container mx-auto md:mx-8">
+            <h2 className="text-xl card bg-gray-800 bg-opacity-50 p-4 rounded-lg mb-4">
+              Key result 1
+            </h2>
+            <h2 className="text-xl card bg-gray-800 bg-opacity-50 p-4 rounded-lg mb-4">
+              Key result 2
+            </h2>
+          </div>
+        </SignedIn>
       </div>
     </main>
   );
